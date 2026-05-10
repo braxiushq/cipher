037cce19a998076d9c3bd982f83e91b3aa86346b
Rishab Yadav <192528458+rishabyd@users.noreply.github.com>
2026-05-11 01:54:39 +0530

    remove buggy dynamic base_url flow

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,10 +2,7 @@
 export const APP_NAME = "cipher";
 export const CONF_PROJECT_NAME = "cipher";
 
-const IS_PROD = process.env.NODE_ENV === "production";
-
-export const DEFAULT_BASE_URL =
-	process.env.CIPHER_API_URL ||
-	(IS_PROD ? "https://api.cipher.braxius.com" : "http://localhost:3000");
+export const DEFAULT_BASE_URL = 
+	process.env.CIPHER_API_URL || "https://api.cipher.braxius.com";
 
 export const API_VERSION = "v1";
